24f73d4aed291db55738900fa748affa8527865c
Ajeibi <ajeibipikin@gmail.com>
2023-04-27 08:39:16 +0100

    innitcommit

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -2,7 +2,6 @@ import { useContext } from 'react';
 
 import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
-import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
   const cartCtx = useContext(CartContext);
